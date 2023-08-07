d6ca4b6b347e4bc77729d36e02c3bdcf81260acf
CayoRoberto <cayoroberto08@gmail.com>
2023-08-07 11:02:13 -0300

    primeira classe posicao

---
+++ src/Programa.java
@@ -0,0 +1,11 @@
+import boardgame.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Posicao pos = new Posicao(3,2);
+		System.out.println(pos);
+	}
+
+}
